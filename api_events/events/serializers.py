d6ff36afa15684261dc0ff8ac2db715086a4893c
Daniel Alexander Cano Cuartas <dacanoc@unal.edu.co>
2020-01-14 13:02:39 -0500

    login y registro

---
+++ api_events/events/serializers.py
@@ -0,0 +1,18 @@
+from rest_framework import serializers
+from rest_framework.validators import UniqueValidator
+from django.contrib.auth.models import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(
+        required=True,
+        validators=[UniqueValidator(queryset=User.objects.all())]
+    )
+    username = serializers.CharField(
+        validators=[UniqueValidator(queryset=User.objects.all())]
+    )
+    password = serializers.CharField(min_length=8)
+
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'email', 'password')
---
+++ api_events/events/views.py
@@ -0,0 +1,20 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.authtoken.models import Token
+from events.serializers import UserSerializer
+
+class UserCreate(APIView):
+    """ 
+    Creates the user. 
+    """
+    def post(self, request, format='json'):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            user.set_password(user.password)
+            user.save()
+            if user:
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
